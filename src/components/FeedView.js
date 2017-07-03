72b9239c1f624fd4863715ad46d9d064bf1edb34
Leisan Kazberova <leisan.kh@gmail.com>
2017-07-03 13:09:13 +0300

    add display title of photo, add ability go to next photo when click by photo in lightbox mode, remove PropTypes warnings.

--- src/components/FeedView.js
+++ src/components/FeedView.js
@@ -1,4 +1,5 @@
-import React, {PropTypes} from 'react';
+import PropTypes from 'prop-types';
+import React from 'react';
 import PhotoGrid from './PhotoGrid';
 import {InfoElement} from './DefaultInfoElement';
 import RadioButtonGroup from './RadioButtonGroup';
